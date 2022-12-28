a75364242b72bb855bcd16442cfa95bda4747834
Devrath Rana <chintusinghrana1234567890@gmail.com>
2022-12-28 16:58:11 +0530

    fetch code update

--- 33_promises_and_fetch.js
+++ 33_promises_and_fetch.js
@@ -1,8 +1,15 @@
-const promiseValue = fetch("./data.json"); //will not return the response right away but will return a PROMISE OBJECT
+const promiseObject1 = fetch("./data.json"); //IT is an API from browser which will not return the response right away but will return a PROMISE OBJECT
 
 //PROMISE OBJECT IS A OBJECT WHICH PROMISES NOT DEFINITELY THAT IT WILL RESOLVE THER REQUEST IN FUTURE with definitive value. LIKE ABOVE IS A PROMISE OBJECT WHICH SAYS IN 2 or 3 seconds I will have a definitive value.
 
 //data here is the resolved value and this then arrow function is run when the said promise is resolved
-promiseValue.then((data) => {
+promiseObject1.then((data) => {
   console.log(data);
+  //   console.log(data.json()); //gives promise object
+
+  const promiseObject2 = data.json();
+
+  promiseObject2.then((res) => {
+    console.log(res);
+  });
 });
